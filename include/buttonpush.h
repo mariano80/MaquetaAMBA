134e045d759174be1581b0af42e29030abca239f
Mariano <m_ucha@hotmail.com>
2023-01-12 17:49:45 -0300

    commit

--- include/buttonpush.h
+++ include/buttonpush.h
@@ -17,6 +17,14 @@ class buttonpush {
 
 public:
 
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 // Constructor de buttonpush
   int buttonpin;
 buttonpush(BUTTON_PARAMS *parameter){
@@ -24,8 +32,31 @@ buttonpush(BUTTON_PARAMS *parameter){
 }
 
 bool states(){
-  return digitalRead(buttonpin);
+  int reading = digitalRead(buttonpin);
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
   }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+
+
+
+}
 
 
 };
