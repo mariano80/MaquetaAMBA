c8589c92fc54f916311e967e6412658a3b67d1dd
Mariano <m_ucha@hotmail.com>
2023-01-20 19:40:04 -0300

    Primer prueba usando milis.

--- include/buttonpush.h
+++ include/buttonpush.h
@@ -17,14 +17,6 @@ class buttonpush {
 
 public:
 
-int buttonState;            // the current reading from the input pin
-int lastButtonState = LOW;  // the previous reading from the input pin
-
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
 // Constructor de buttonpush
   int buttonpin;
 buttonpush(BUTTON_PARAMS *parameter){
